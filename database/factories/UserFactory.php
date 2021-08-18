dcff0b7f144079b27180cf7dfd370fc077dd0805
Zero One <myzerone2@gmail.com>
2021-08-18 06:14:13 -0400

    ready to push

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,17 +23,12 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'name' => 'Milad',
-            'email' => 'milad@mindwingx.io',
+            'name' => 'Admin',
+            'email' => 'admin@test.io',
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'role' => 'admin',
             'remember_token' => Str::random(10),
-            /*'name' => $this->faker->name(),
-            'email' => $this->faker->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),*/
         ];
     }
 
